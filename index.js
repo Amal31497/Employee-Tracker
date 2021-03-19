fcca8ba839cf07caefb0a06aa2f6c3a01c975298
Amal Janabayev <amalj2426@gmail.com>
2021-03-18 23:43:20 -0700

    Added all queries, going to add readme and walkthrough in the next commit

--- index.js
+++ index.js
@@ -15,6 +15,45 @@ const connection = mysql.createConnection({
   database: 'organization_db',
 });
 
+const start = () => {
+    inquirer
+    .prompt({
+    name:"EMS",
+    type:"list",
+    message:"What would you like to do?",
+    choices:["View all employees","View all employees by Department","Add Employee","View all Departments","View all Roles","Add Department","Add Role","Exit"]
+    })
+    .then((answer) => {      
+        switch(answer.EMS){
+            case 'View all employees':
+                allEmployees();
+                break;
+            case 'View all employees by Department':
+                allEmpByDep();
+                break;
+            case 'Add Employee':
+                addEmployee();
+                break;
+            case 'View all Departments':
+                allDepartments();
+                break;
+            case 'View all Roles':
+                allRoles();
+                break;
+            case 'Add Department':
+                addDepartment();
+                break;
+            case 'Add Role':
+                addRole();
+                break;
+            case 'Exit':
+                process.exit();
+                break;
+        }
+    })
+}
+
+
 const init = () => {
     console.log(`                                                 
   ____            _            _____ _ ___ ___ _ _     
@@ -39,67 +78,16 @@ const init = () => {
         |_____|_  |___|_| |___|_|_|_|
               |___|                  
 `);
-
-    inquirer
-    .prompt({
-    name:"EMS",
-    type:"list",
-    message:"What would you like to do?",
-    choices:["View all employees","View all employees by Department","Add Employee","Remove employee","Update employee role"]
-    })
-    .then((answer) => {      
-        switch(answer.EMS){
-            case 'View all employees':
-                allEmployees();
-                break;
-            case 'View all employees by Department':
-                allEmpByDep();
-                break;
-            case 'Add Employee':
-                addEmployee();
-                break;
-            case 'Remove Employee':
-                removeEmployee();
-                break;
-            // case 'Update employee Role':
-            //     updateEmpRole();
-            //     break;
-            // case 'Update employee Manager':
-            //     updateEmpManager();
-            //     break;
-            // case 'View all roles':
-            //     allRoles();
-            //     break;
-            // case 'View all roles by Department':
-            //     allRolesByDep();
-            //     break;
-            // case 'Add Role':
-            //     addRole();
-            //     break;
-            // case 'Remove Role':
-            //     removeRole();
-            //     break;
-            // case 'View all Departments':
-            //     allDep();
-            //     break;
-            // case 'Add Department':
-            //     addDep();
-            //     break;
-            // case 'Remove Department':
-            //     removeDep();
-            //     break;
-            // case 'Total utilized Budget by Department':
-            //     budgetByDep();
-            //     break;
-        }
-    })
+start();
 }
+    
 var allEmployees = () => {
     connection.query('SELECT * FROM employee', (err, res) => {
         if (err) throw err;
         console.table(res);
-        connection.end();
-    });
+        //connection.end();
+        start();
+    });  
 };
 
 var allEmpByDep = () => {
@@ -107,7 +95,8 @@ var allEmpByDep = () => {
         (err,res) => {
             if(err) throw err;
             console.table(res);
-            connection.end()
+            //connection.end();
+            start();
         }
     )
 }
@@ -133,13 +122,99 @@ var addEmployee = () => {
         }
       ])
       .then((answer) => {
-          connection.query(`INSERT INTO employee (first_name,last_name,role_id) VALUES(${answer.firstName},${answer.lastName},(SELECT role_id FROM company_role WHERE title=${answer.title}))`,
+          connection.query(`INSERT INTO employee (first_name,last_name,role_id) VALUES(?,?,(SELECT role_id FROM company_role WHERE title=?))`,
+          [
+              answer.firstName,
+              answer.lastName,
+              answer.title
+          ],  
           (err, res) => {
-              if(err) throw err;
-              console.log("Your new employee added!");
-          }
+                if(err) throw err;
+                console.log("Your new employee added!");
+                //connection.end();
+                start();
+            }
           )
       })
+}
+
+var allDepartments = () => {
+    connection.query("SELECT department_name FROM department", (err, res) => {
+        if(err) throw err;
+        console.log("These are all departments we have her at Scranton branch!");
+        console.table(res);
+        start();
+    })
+}
+
+var addDepartment = () => {
+    inquirer.prompt([
+        {
+            name:"depName",
+            type:"input",
+            message:"What is the name your new department?"
+        }
+    ])
+    .then((answer) => {
+        connection.query("INSERT INTO department (department_name) VALUES(?)",
+        [answer.depName],
+        (err, res) => {
+            if(err) throw err;
+            console.log("Your department was added!");
+            start();
+        }
+        )
+    })
+}
+
+var addRole = () => {
+    connection.query("SELECT department_name FROM department",(err,res)=>{
+        if(err) throw err;
+        inquirer
+      .prompt([
+          {
+            name:"title",
+            type:"input",
+            message:"What is the name of this new role you would like to add?"          
+          },
+          {
+            name:"salary",
+            type:"input",
+            message:"Select a salary for this role please"
+          },
+          {
+            name:"depName",
+            type:"list",
+            message:"Please select the department you are adding this new role to!",
+            choices(){
+                var deptArray = [];
+                res.forEach(({ department_name }) => {
+                    deptArray.push(department_name);
+                });
+                return deptArray;
+            }
+          }
+      ])
+      .then((answers) => {
+        connection.query(`INSERT INTO company_role (title, salary, department_id) VALUES(${answers.title},${answers.salary},(SELECT department_id FROM department WHERE department_name=${answers.depName}))`,
+            (err,res) => {
+                if(err) throw err;
+                console.log("The new role was successfully added!");
+                start();
+            }
+        )
+      })
+    })
+    
+}
+
+var allRoles = () => {
+    connection.query("SELECT title,salary FROM company_role",(err, res) => {
+        if(err) throw err;
+        console.log("These are all of our roles we have at Dunder Mifflin in Scranton!");
+        console.table(res);
+        start();
+    })
 }
 
 connection.connect((err) => {
